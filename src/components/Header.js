ccd0bcbd8ac7750c2a08cf3ef9c9373ef2235717
GiJoong Jang <rlwnd0000@naver.com>
2022-06-10 13:55:01 +0900

    Update Header.js
    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ function Header({isAuthenticated, setIsAuthenticated}) {
                 </div>
                 <ul className="navbar-nav justify-content-end container">
                     <li className="nav-link px-4"><Link to='/'>Home</Link></li>
-                    {isAuthenticated && <li className="nav-link px-4"><Link to='/todoList'>View Todo</Link></li>}
-                    {isAuthenticated && <li className="nav-link px-4"><Link to='/logout'>logout</Link></li>}
-                    {!isAuthenticated && <li className="nav-link px-4"><Link to='/signin'>Signin</Link></li>}
-                    {!isAuthenticated && <li className="nav-link px-4"><Link to='/signup'>Signup</Link></li>}
+                    {isAuthenticated && <li className="nav-link px-4"><Link to='/todoList'>Todo</Link></li>}
+                    {isAuthenticated && <li className="nav-link px-4"><Link to='/logout'>로그아웃</Link></li>}
+                    {!isAuthenticated && <li className="nav-link px-4"><Link to='/signin'>로그인</Link></li>}
+                    {!isAuthenticated && <li className="nav-link px-4"><Link to='/signup'>회원가입</Link></li>}
                 </ul>
             </nav>
 		</header>
